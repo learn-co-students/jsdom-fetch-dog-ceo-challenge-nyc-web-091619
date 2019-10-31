43b46dc4d69f03e4e747d9f5097887ed90e9ec67
Danny Lee <dreamer@panix.com>
2019-10-30 20:44:25 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchBooks() {
+    const newLocal = imgUrl;
+    return fetch(newLocal)
+    .then(resp => resp.json())
+    // .then(json => data = json);
+    .then(json => renderBooks(json));
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks()
+  })
